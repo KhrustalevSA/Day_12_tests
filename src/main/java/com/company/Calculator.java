d503ce1f7a65793f7c9f87f14f6f9de15a0b2eba
Сергей Хрусталев <serega.135246@mail.ru>
2021-07-11 13:53:11 +0300

    Мавен запустил тесты

--- src/main/java/com/company/Calculator.java
+++ src/main/java/com/company/Calculator.java
@@ -23,21 +23,4 @@ public class Calculator {
         }
     }
 
-
-    public int sum_error(int num1, int num2){           // Не работающие методы калькулятора
-        int sum_error = num1 * num2;
-        return sum_error;
-    }
-    public int sub_error(int num1, int num2){
-        int sub_error = num1 + num2;
-        return sub_error;
-    }
-    public int mul_error(int num1, int num2){
-        int mul_error = num1 * num2;
-        return mul_error*2;
-    }
-    public int div_error(int num1, int num2){
-        int div_error = num1 / num2 * 2;
-        return div_error;
-    }
 }
--- src/test/java/com/company/CalculatorTest.java
+++ src/test/java/com/company/CalculatorTest.java
@@ -56,40 +56,4 @@ class CalculatorTest {
         }
     }
 
-    @Test
-    void sum_error() {                                      // Тесты не работающих методов
-        Calculator calculator = new Calculator();
-
-
-        int sum = calculator.sum_error(5,5);
-
-        assertEquals(5+5,sum);
-    }
-
-    @Test
-    void sub_error() {
-        Calculator calculator = new Calculator();
-
-        int sub = calculator.sub_error(9,3);
-
-        assertEquals(9-3,sub);
-    }
-
-    @Test
-    void mul_error() {
-        Calculator calculator = new Calculator();
-
-        int mul = calculator.mul_error(2,2);
-
-        assertEquals(2*2,mul);
-    }
-
-    @Test
-    void div_error() {
-        Calculator calculator = new Calculator();
-
-        int div = calculator.div_error(50,5);
-
-        assertEquals(50/5,div);
-    }
 }
